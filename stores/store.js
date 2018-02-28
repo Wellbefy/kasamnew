886d1422c8c6cc7f228c37d779eb0bfc49f24832
Mohammad <mohammad.dabour89@yahoo.com>
2018-02-28 10:50:08 +0100

    min andra commit

--- stores/store.js
+++ stores/store.js
@@ -3,12 +3,7 @@ import Home from '/Users/Mido/tuff/tuffOne/Screens/Home.js'
 
 class store {
     @observable all = [
-        {value1: ' ' },
-        {value: 2, label: '2'},
-        {value: 3, label: '3'},
-        {value: 4, label: '4'},
-        {value: 5, label: '5'},
-        {value: 6, label: '6',}
+        {value1: ' ' }
         ];
 }
 export default new store();
